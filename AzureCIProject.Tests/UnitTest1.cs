67008c1ec98b8ac5245772ee50319ec482dcbc90
Neteazy <gary@neteazy.com>
2018-08-08 03:13:44 -0400

    Add project files.

---
+++ AzureCIProject.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AzureCIProject.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
